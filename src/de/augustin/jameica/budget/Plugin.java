d38cc71838e6ca4164adead28588c7969be4a55b
abc2006 Laptop <abc2006@gmx.de>
2012-11-04 15:52:02 +0100

    Added Files existing on Laptop

---
+++ src/de/augustin/jameica/budget/Plugin.java
@@ -0,0 +1,11 @@
+package de.augustin.jameica.budget;
+
+import de.willuhn.jameica.plugin.AbstractPlugin; 
+
+
+public class Plugin extends AbstractPlugin
+{
+	//hier können verschiedene Funktionen wie init, Install und Clean aufgerufen werden
+	
+	
+}
---
+++ src/de/augustin/jameica/budget/Settings.java
@@ -0,0 +1,13 @@
+package de.augustin.jameica.budget;
+
+import de.willuhn.jameica.system.Application;
+
+public class Settings
+{
+	
+	private final static de.willuhn.jameica.system.Settings settings = Application.getPluginLoader().getPlugin(Plugin.class).getResources().getSettings();
+	
+	
+	
+	
+}
