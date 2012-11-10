f75005cbcfd5bf2f1011491e7571cc6cd9bac848
abc2006 <abc2006@gmx.de>
2012-11-10 14:52:13 +0100

    handleStore() hinzugefügt.. auch wenn ich noch nciht weiss, wies aufgerufen wird ...

--- src/de/augustin/jameica/budget/gui/control/SummaryControl.java
+++ src/de/augustin/jameica/budget/gui/control/SummaryControl.java
@@ -1,6 +1,7 @@
 package de.augustin.jameica.budget.gui.control;
 
 import java.rmi.RemoteException;
+import java.util.Date;
 
 
 import de.willuhn.datasource.rmi.DBIterator;
@@ -14,6 +15,10 @@ import de.willuhn.jameica.gui.formatter.CurrencyFormatter;
 import de.willuhn.jameica.gui.formatter.DateFormatter;
 import de.willuhn.jameica.gui.formatter.Formatter;
 import de.willuhn.jameica.gui.parts.TablePart;
+import de.willuhn.jameica.messaging.StatusBarMessage;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.logging.Logger;
+import de.willuhn.util.ApplicationException;
 
 
 
@@ -25,13 +30,33 @@ import de.willuhn.jameica.gui.parts.TablePart;
 
 public class SummaryControl extends AbstractControl
 {
+	
+	// this is the currently opened car
+	private Car car;
+
+	  
 	public SummaryControl(AbstractView view) {
 		super(view);
 	}
 
+	  /**
+	   * Small helper method to get the current car.
+	   * @return
+	   */
+	  private Car getCar()
+	  {
+	    if (car != null)
+	      return car;
+	    car = (Car) getCurrentObject();
+	    return car;
+	  }
+	
+	
+	
+	
 	private TablePart projectList;
 	
-	public Part getProjectsTable() throws RemoteException
+	public Part getCarTable() throws RemoteException
 	  {
 		
 		// 1) get the dataservice
@@ -75,7 +100,52 @@ public class SummaryControl extends AbstractControl
 		
 	  }
 	
-	
+	 public void handleStore()
+	  {
+	    try
+	    {
+
+	      // get the current project.
+	      Car p = getCar();
+
+	      // invoke all Setters of this project and assign the current values
+	      //p.setName((String) getName().getValue());
+	      //p.setDescription((String) getDescription().getValue());
+	      //p.setFuelDate(to"1.1.2012");
+	      p.setKm_Total(1000);
+	      p.setKm_Trip(200);
+	      p.setPrice_Liter(1.99);
+	      p.setPrice_Total(24.25);
+	      p.setComment("Kommentar");
+	      p.setStation("Total AB");
+	      p.setConsumption(6.33);
+				// we can cast the return value of date input directly to "java.util.Date".
+	//      p.setEndDate((Date) getEndDate().getValue());
+	//      p.setStartDate((Date) getStartDate().getValue());
+
+				// the DecimalInput fields returns a Double object
+	//      Double d = (Double) getPrice().getValue();
+	//      p.setPrice(d == null ? 0.0 : d.doubleValue());
+
+	      // Now, let's store the project
+	      // The store() method throws ApplicationExceptions if
+	      // insertCheck() or updateCheck() failed.
+	      try
+	      {
+	        p.store();
+	        Application.getMessagingFactory().sendMessage(new StatusBarMessage(Settings.i18n().tr("Project stored successfully"),StatusBarMessage.TYPE_SUCCESS));
+	      }
+	      catch (ApplicationException e)
+	      {
+	        Application.getMessagingFactory().sendMessage(new StatusBarMessage(e.getMessage(),StatusBarMessage.TYPE_ERROR));
+	      }
+	    }
+	    catch (RemoteException e)
+	    {
+	      Logger.error("error while storing project",e);
+	      Application.getMessagingFactory().sendMessage(new StatusBarMessage(Settings.i18n().tr("Error while storing Project: {0}",e.getMessage()),StatusBarMessage.TYPE_ERROR));
+	    }
+	  }
 	
 	
 	
