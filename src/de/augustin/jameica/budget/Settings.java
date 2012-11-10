0121a906d1fb3963c2ba5878ed6a74f088dffea4
abc2006 <abc2006@gmx.de>
2012-11-10 12:11:56 +0100

    DB-Service hinzugefügt - nicht getestet

--- src/de/augustin/jameica/budget/Settings.java
+++ src/de/augustin/jameica/budget/Settings.java
@@ -14,6 +14,7 @@ public class Settings
 {
 	
 	private static I18N i18n;
+	private static DBService db;
 	
 	private final static de.willuhn.jameica.system.Settings settings = Application.getPluginLoader().getPlugin(Plugin.class).getResources().getSettings();
 	
@@ -44,5 +45,30 @@ public class Settings
 				return i18n;
 			i18n = Application.getPluginLoader().getPlugin(Plugin.class).getResources().getI18N();
 			return i18n; 
+		}
+		/**
+		 * Small helper function to get the database service.
+	   * @return db service.
+	   * @throws RemoteException
+	   */
+	  public static DBService getDBService() throws RemoteException
+		{
+			if (db != null)
+				return db;
+
+			try
+			{
+				// We have to ask Jameica's ServiceFactory.
+				// If we are running in Client/Server mode and we are the
+				// client, the factory returns the remote dbService from the
+				// Jameica server.
+				// The name and class of the service is defined in plugin.xml
+				db = (DBService) Application.getServiceFactory().lookup(Plugin.class,"exampledatabase");
+				return db;
+			}
+			catch (Exception e)
+			{
+				throw new RemoteException("error while getting database service",e);
+			}
 		}
 }
