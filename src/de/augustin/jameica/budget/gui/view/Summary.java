9cfc72509b3cb76beab69b721d0dcaf08c60f828
abc2006 <abc2006@gmx.de>
2012-11-10 10:10:42 +0100

    Files angepasst, um eine Tabelle zu zeigen

--- src/de/augustin/jameica/budget/gui/view/Summary.java
+++ src/de/augustin/jameica/budget/gui/view/Summary.java
@@ -3,7 +3,10 @@ package de.augustin.jameica.budget.gui.view;
 
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.GUI;
+import de.willuhn.jameica.gui.util.ColumnLayout;
+import de.willuhn.jameica.gui.util.Container;
 import de.willuhn.jameica.gui.util.LabelGroup;
+import de.willuhn.jameica.gui.util.SimpleContainer;
 
 
 public class Summary extends AbstractView
@@ -18,12 +21,25 @@ public class Summary extends AbstractView
         LabelGroup group = new LabelGroup(this.getParent(),"welcome");
 		
 		group.addText("this page intentionally left blank ;)",false);
+		//################################## Ab hier wird eine zweispaltige Tabelle erstellt. Dies 
+		// hat nichts mit dem Kopf zu tun ! 
 		
+	    Container c = new SimpleContainer(getParent());
+
+	    // layout with 2 columns
+	    ColumnLayout columns = new ColumnLayout(c.getComposite(),2);
+	    	// left side
+	    	Container left = new SimpleContainer(columns.getComposite());
+	    	left.addHeadline("meine Headline");
 		
+	    	// right side
+	    	Container right = new SimpleContainer(columns.getComposite(),true);
+	    	right.addHeadline("Description");
 		
-		
-		
-	}
+	    // Zurück zum Container itself:
+	    c.addHeadline("Summary");
+	    // ##########leider wird die Tabelle selbst noch nicht ausgegeben
+		}// public void bind
 	
 	
 	
