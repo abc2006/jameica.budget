55f8bf9a577ee0054792c4bb316caa2b9189819d
abc2006 <abc2006@gmx.de>
2012-11-10 12:35:29 +0100

    Install-Funktion zur Erstellung einer Datenbank beim ersten start? hinzugefügt

--- src/de/augustin/jameica/budget/Plugin.java
+++ src/de/augustin/jameica/budget/Plugin.java
@@ -1,11 +1,46 @@
 package de.augustin.jameica.budget;
+import java.io.File;
+
+import de.willuhn.datasource.db.EmbeddedDatabase;
+import de.augustin.jameica.budget.Plugin;
 // diese Datei initialisiert das Plugin. Sie ruft init, install und/oder clean auf. Danach ist diese Datei ohne funktion.
 import de.willuhn.jameica.plugin.AbstractPlugin; 
+import de.willuhn.jameica.plugin.Manifest;
+import de.willuhn.jameica.plugin.PluginResources;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.util.ApplicationException;
 
 
 public class Plugin extends AbstractPlugin
 {
 	//hier können verschiedene Funktionen wie init, Install und Clean aufgerufen werden
-	
+	  
+	  public void install() throws ApplicationException
+	  {
+
+			// If we are running in client/server mode and this instance
+			// is the client, we do not need to create a database.
+			// Instead of this we will get our objects via RMI from
+			// the server
+			if (Application.inClientMode())
+				return;
+
+	    try {
+
+				// Let's create an embedded Database
+				PluginResources res = Application.getPluginLoader().getPlugin(Plugin.class).getResources();
+				Manifest mf = Application.getPluginLoader().getManifest(Plugin.class);
+				EmbeddedDatabase db = new EmbeddedDatabase(res.getWorkPath() + "/db","budgetuser","budgetpassword");
+
+				// create the sql tables.
+	      db.executeSQLScript(new File(mf.getPluginDir() + File.separator + "sql","create.sql"));
+
+				// That's all. Database installed and tables created ;)
+	    }
+	    catch (Exception e)
+	    {
+	    	throw new ApplicationException("error while installing plugin",e);
+	    }
+	  }
 	
 }
