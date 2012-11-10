86385485b53fc1f2349b2e6f198dff33423e4103
abc2006 <abc2006@gmx.de>
2012-11-10 12:32:11 +0100

    Datenbank in PluginXML aktiviert und Interface bereitgestellt

---
+++ src/de/augustin/jameica/budget/rmi/BudgetDBService.java
@@ -0,0 +1,11 @@
+package de.augustin.jameica.budget.rmi;
+
+import de.willuhn.datasource.rmi.DBService;
+
+/**
+ * Interface for our database service.
+ */
+public interface BudgetDBService extends DBService
+{
+
+}
