4b6c21e1232ec30f0b2e9db345050f3d28932b52
abc2006 <abc2006@gmx.de>
2012-11-15 12:21:20 +0100

    a

--- sql/create.sql
+++ sql/create.sql
@@ -1,11 +1,7 @@
-CREATE TABLE car (
-  id NUMERIC default UNIQUEKEY('car'),
-  fueldate date NOT NULL,
-  km_trip double,
-  km_total double NOT NULL,
-  price_liter double,
-  price_total double,
-  consumption double,
-  station text,
-  comments text
+CREATE TABLE test (
+	id NUMERIC default UNIQUEKEY('test'),
+	buchstabensalat text,
+	UNIQUE (id),
+	PRIMARY KEY (id)
 );
+
