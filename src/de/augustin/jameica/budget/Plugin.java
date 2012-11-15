c4097a1b6713a35fd59c82a774aab9b2dbffa361
abc2006 <abc2006@gmx.de>
2012-11-15 12:11:07 +0100

    Großer Umbau auf funktionierendes "neu"-Projekt

--- src/de/augustin/jameica/budget/Plugin.java
+++ src/de/augustin/jameica/budget/Plugin.java
@@ -1,20 +1,22 @@
+// dies ist die initiale Plugin-Datei. Sie wird von Jameica aufgerufen, um das Plugin zu starten
+
 package de.augustin.jameica.budget;
+
+
+
+// diese Datei initialisiert das Plugin. Sie ruft init, install und/oder clean auf. Danach ist diese Datei ohne funktion.
 import java.io.File;
 
 import de.willuhn.datasource.db.EmbeddedDatabase;
-import de.augustin.jameica.budget.Plugin;
-// diese Datei initialisiert das Plugin. Sie ruft init, install und/oder clean auf. Danach ist diese Datei ohne funktion.
 import de.willuhn.jameica.plugin.AbstractPlugin; 
 import de.willuhn.jameica.plugin.Manifest;
 import de.willuhn.jameica.plugin.PluginResources;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.util.ApplicationException;
-
+import de.augustin.jameica.budget.Plugin;
 
 public class Plugin extends AbstractPlugin
 {
-	//hier können verschiedene Funktionen wie init, Install und Clean aufgerufen werden
-	  
 	  public void install() throws ApplicationException
 	  {
 
@@ -30,7 +32,7 @@ public class Plugin extends AbstractPlugin
 				// Let's create an embedded Database
 				PluginResources res = Application.getPluginLoader().getPlugin(Plugin.class).getResources();
 				Manifest mf = Application.getPluginLoader().getManifest(Plugin.class);
-				EmbeddedDatabase db = new EmbeddedDatabase(res.getWorkPath() + "/db","budgetuser","budgetpassword");
+				EmbeddedDatabase db = new EmbeddedDatabase(res.getWorkPath() + "/db","neueruser","neuespassword");
 
 				// create the sql tables.
 	      db.executeSQLScript(new File(mf.getPluginDir() + File.separator + "sql","create.sql"));
@@ -42,5 +44,5 @@ public class Plugin extends AbstractPlugin
 	    	throw new ApplicationException("error while installing plugin",e);
 	    }
 	  }
-	
+
 }
